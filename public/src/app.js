42e89217cd41ce8886da689f04e889947545e011
KostadinStefanov <kosstefanov@gmail.com>
2015-01-20 20:55:46 +0200

    added .gitignore

---
+++ public/src/app.js
@@ -0,0 +1,17 @@
+angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/contacts', {
+                controller: 'ListController',
+                templateUrl: 'views/list.html'
+            })
+            .when('/contact/new', {
+                controller: 'NewController',
+                templateUrl: 'views/new.html'
+            })
+           /* .otherwise({
+                redirectTo: '/contacts'   
+            }); */
+        $locationProvider.html5Mode(true);
+        })
+   
